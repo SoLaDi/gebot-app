e1cb09a2f7928013bdd9cbdfaf26f40f1846577b
romainslab <romain@posteo.de>
2022-02-17 22:25:27 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'application#index'
+
+  get '/mitgliedschaften/:id', to: 'memberships#show', as: 'membership'
+  put '/mitgliedschaften/:id', to: 'memberships#edit'
   get '/404', to: 'errors#not_found', as: 'not_found'
 end
