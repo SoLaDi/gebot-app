69ddac185f9bb9a67a95335689c2779b319cb3f7
romainslab <49669882+romainslab@users.noreply.github.com>
2022-02-27 23:03:29 +0100

    Implement post api call (#3)
    * Remove unused SOLADI_API_LOGIN_PATH env
    
    * Add id to membership model
    
    * Implement post call to soladi

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,12 +4,12 @@ class MembershipsController < ApplicationController
     response = conn.get('/api/magic_link/login', { token: params[:id] }, headers)
 
     if response.status != 200
-      # raise
       redirect_to not_found_path
     else
       data = JSON.parse(response.body, symbolize_names: true)
 
       @membership = Membership.new({
+        id: data[:membership][:id],
         name: [data[:name], data[:surname]].join(' '),
         amount: data[:membership][:bids][0][:amount],
       })
@@ -17,21 +17,37 @@ class MembershipsController < ApplicationController
   end
 
   def edit
-    if Rails.env.development?
-      @membership = Membership.new({ name: 'Max Müller' })
-      @membership.attributes = update_params
+    body = {
+      bid: {
+        start_date: "2022-04-01",
+        end_date: "2023-03-31",
+        membership_id: update_params[:id].to_i,
+        amount: update_params[:amount].to_f,
+        shares: 2,
+      }
+    }
+
+    response = conn.post('/api/bids', body.to_json, headers)
+
+    if response.status == 202
+      flash[:notice] = 'Gebot aktualisiert!'
     else
-      # call soladi api to put this membership
+      flash[:notice] = JSON.parse(response.body)['base'].join(' ')
     end
 
-    flash[:notice] = 'Gebot aktualisiert!'
+    @membership = Membership.new({
+      id: update_params[:id].to_i,
+      name: update_params[:name],
+      amount: update_params[:amount].to_f,
+    })
+
     render :show
   end
 
   private
 
   def update_params
-    params.require(:membership).permit(:amount)
+    params.require(:membership).permit(:amount, :id, :name)
   end
 
   def conn
