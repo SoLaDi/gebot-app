ab064fde92457aad9c3e7c1e453d462919eb4fa7
romainslab <49669882+romainslab@users.noreply.github.com>
2022-03-03 16:49:04 +0100

    Use person email returned by magic auth (#5)

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,6 +3,7 @@ class Membership
 
   attr_accessor :id, :integer
   attr_accessor :name, :string
+  attr_accessor :email, :string
   attr_accessor :amount, :float
   attr_accessor :shares, :int
   attr_accessor :has_current_bid, :bool
