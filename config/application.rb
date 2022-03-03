fdddfce89d63f87009f0e4982963e1bc72634b7b
romainslab <romain@posteo.de>
2022-03-03 12:41:59 +0100

    Set locale to de

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,11 @@ module GebotApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Permitted locales available for the application
+    I18n.available_locales = [:en, :de]
+
+    # Set default locale to something other than :en
+    I18n.default_locale = :de
   end
 end
