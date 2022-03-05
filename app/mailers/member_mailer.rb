f6673e83ad1568d997850367c7cfe4082f77f010
Marco Hoyer <3943275+marco-hoyer@users.noreply.github.com>
2022-03-05 16:27:25 +0100

    add bank details to confirmation mail (#6)
    * add bank details to confirmation mail
    
    * Update member_mailer.rb

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -2,6 +2,7 @@ class MemberMailer < ApplicationMailer
   def notify_bid
     @name = params[:name]
     @bid = params[:bid]
-    mail(to: params[:email], subject: 'Dein Mitgliedsbeitrag wurde erfolgreich gespeichert')
+    @membership_id = params[:membership_id]
+    mail(to: params[:email], subject: 'Dein Gebot wurde erfolgreich gespeichert')
   end
 end
--- test/mailers/previews/member_mailer_preview.rb
+++ test/mailers/previews/member_mailer_preview.rb
@@ -1,5 +1,5 @@
 class MemberMailerPreview < ActionMailer::Preview
   def notify_bid
-    MemberMailer.with(name: 'Max Müller', email: 'max.mueller@gmx.de', bid: 98.5).notify_bid
+    MemberMailer.with(name: 'Max Müller', email: 'max.mueller@gmx.de', bid: 98.5, membership_id: 1234).notify_bid
   end
 end
